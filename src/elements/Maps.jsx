0d2bfe4feade820bb808fbf65edfb76ae9da0da9
AndriySuprunenko <43989070+AndriySuprunenko@users.noreply.github.com>
2022-11-30 22:07:52 +0200

    maps

--- src/elements/Maps.jsx
+++ src/elements/Maps.jsx
@@ -1,4 +1,5 @@
 // Imports
+import { useMemo } from 'react';
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 // Scss
 import '../styles/elements/maps.scss';
@@ -13,20 +14,14 @@ export default function Maps() {
 }
 
 function Map() {
+  const center = useMemo(
+    () => ({ lat: 49.066548193739635, lng: 33.42403396608896 }),
+    []
+  );
+
   return (
-    <GoogleMap
-      zoom={18}
-      center={{ lat: 49.066548193739635, lng: 33.42403396608896 }}
-      mapContainerClassName="map-container"
-    >
-      <Marker position={{ lat: 49.066548193739635, lng: 33.42403396608896 }} />
+    <GoogleMap zoom={18} center={center} mapContainerClassName="map-container">
+      <Marker position={center} />
     </GoogleMap>
   );
 }
-
-// https://maps.gooleapis.com/maps/api/js?key=AIzaSyBbt0F3RGczTmbyVMMyFIVd8wxxFSpFRN0&region=EN&language=en&callback=initMap&v=weekly
-// center: {
-//   lat: 49.066548193739635,
-//   lng: 33.42403396608896,
-// },
-// zoom: 11,
